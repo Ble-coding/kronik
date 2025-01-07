2ca6e931f59749de639d4cdb0d41b7fa6f8ed57a
Levis Ble <levisble@gmail.com>
2025-01-07 14:28:50 +0000

    lgo partner

--- layouts/about/partner.php
+++ layouts/about/partner.php
@@ -2,19 +2,22 @@
             <div class="container" data-aos="fade-up">
                 <div class="box-logos-partner box-logos-partner-5-col">
                     <div class="item-partner">
-                        <img src="assets/imgs/pages/home5/doo.png" alt="kronik" />
+                        <img src="assets/imgs/pages/home5/ordre_medecin.png" alt="kronik" />
                     </div>
                     <div class="item-partner text-center">
-                        <img src="assets/imgs/pages/home5/inst.png" alt="kronik" />
+                        <img src="assets/imgs/pages/home5/pass_mousso.png" alt="kronik" />
                     </div>
                     <div class="item-partner text-center">
-                        <img src="assets/imgs/pages/home5/invision.png" alt="kronik" />
+                        <img src="assets/imgs/pages/home5/sgci.png" alt="kronik" />
                     </div>
                     <div class="item-partner text-center">
-                        <img src="assets/imgs/pages/home5/hellosign.png" alt="kronik" />
+                        <img src="assets/imgs/pages/home5/pnud.png" alt="kronik" />
                     </div>
                     <div class="item-partner text-end">
-                        <img src="assets/imgs/pages/home5/dribbble.png" alt="kronik" />
+                        <img src="assets/imgs/pages/home5/roche.png" alt="kronik" />
+                    </div>
+                    <div class="item-partner text-end">
+                        <img src="assets/imgs/pages/home5/ministere_sante.png" alt="kronik" />
                     </div>
                 </div>
             </div>
