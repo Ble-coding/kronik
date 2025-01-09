a55b730bba06557783f978573b937e5391cf5289
Levis Ble <levisble@gmail.com>
2025-01-09 15:25:05 +0000

    pârtner logo géré

---
+++ languages/en/about/partner.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'supported_and_accompanied_by' => 'SUPPORTED AND ACCOMPANIED BY',
+    'our_partners' => 'OUR PARTNERS',
+    'founding_members' => 'FOUNDING MEMBERS',
+    'members' => 'MEMBERS',
+];
