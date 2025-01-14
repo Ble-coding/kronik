29f4dc8fce8e73fd8c0a0b0bf467a4b605ff66bb
Levis Ble <levisble@gmail.com>
2025-01-14 15:02:03 +0000

    about pour le texte home

--- languages/fr/home/about.php
+++ languages/fr/home/about.php
@@ -2,6 +2,6 @@
 return [
     'small_title' => "Engagés pour un avenir en santé numérique",
     'heading' => "Investir dans l'innovation pour <span class=\"theme-primary underline\">transformer la santé</span>",
-    'paragraph' => "Kronik-X Health, incubateur dédié à l'innovation en santé numérique, transforme la gestion des maladies chroniques dans les pays à revenu faible et intermédiaire (LMICs). Avec 85% de la population mondiale, ces régions font face à des défis comme l’augmentation des maladies chroniques et un accès limité aux soins. Nos initiatives, telles que l’incubation de startups et le lancement de projets pilotes, visent à apporter des solutions mesurables tout en promouvant l’impact social, l’accessibilité, et l’innovation.",
+    'paragraph' => "Kronik-X Health, accelerateur et incubateur dédié à l'innovation en santé numérique, transforme la gestion des maladies chroniques dans les pays à revenu faible et intermédiaire (LMICs). Avec 85% de la population mondiale, ces régions font face à des défis comme l’augmentation des maladies chroniques et un accès limité aux soins. Nos initiatives, telles que l’incubation de startups et le lancement de projets pilotes, visent à apporter des solutions mesurables tout en promouvant l’impact social, l’accessibilité, et l’innovation.",
     'cta_button' => "Découvrir nos initiatives",
 ];
