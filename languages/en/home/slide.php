df7071c3ceeb48ffe1b7386b7eeee32358937d00
Levis Ble <levisble@gmail.com>
2025-01-17 10:13:36 +0000

    mise a jour texte home

--- languages/en/home/slide.php
+++ languages/en/home/slide.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'banner_small_title' => 'Innovate to transform',
-    'banner_title' => 'Chronic disease management<br class="d-none d-lg-block" />in Africa and <span class="bg-blue">LMICs</span>',
+    'banner_title' => 'Chronic disease management <br class="d-none d-lg-block" /> in Africa and <span class="bg-blue">LMICs</span>',
     'banner_description' => 'Kronik-X Health, the first digital innovation accelerator and incubator, is committed to creating impactful and accessible solutions for chronic diseases such as diabetes, hypertension, cancer, and mental illnesses.',
     'discover_mission' => 'Discover our mission',
     'need_help' => 'Need help?',
--- languages/fr/home/slide.php
+++ languages/fr/home/slide.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'banner_small_title' => 'Innover pour transformer',
-    'banner_title' => 'La gestion des maladies chroniques<br class="d-none d-lg-block" />en Afrique et dans les <span class="bg-blue">LMICs</span>',
+    'banner_title' => 'La gestion des maladies chroniques <br class="d-none d-lg-block" /> en Afrique et dans les <span class="bg-blue">LMICs</span>',
     'banner_description' => 'Kronik-X Health, le premier accélérateur et incubateur dédié à l’innovation numérique, s’engage à créer des solutions impactantes et accessibles pour les maladies chroniques telles que le diabète, l’hypertension, le cancer et les maladies mentales.',
     'discover_mission' => 'Découvrez notre mission',
     'need_help' => 'Besoin d\'aide?',
