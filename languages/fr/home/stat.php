22b06a381d0f90b14bae44e3e0e0c427f7571478
Levis Ble <levisble@gmail.com>
2025-01-18 15:30:07 +0000

    Agencement position stat pour la page home

--- languages/fr/home/stat.php
+++ languages/fr/home/stat.php
@@ -6,12 +6,12 @@ return [
     'learn_more' => 'En savoir plus',
     'cards' => [
         [
-            'title' => 'Hypertension : Un Diagnostic Crucial',
-            'content' => 'En Afrique subsaharienne, une majorité alarmante de personnes souffrant d\'hypertension restent non diagnostiquées. Ce manque de diagnostic expose des millions de vies à des risques accrus de complications graves, notamment les maladies cardiovasculaires et les accidents vasculaires cérébraux.',
-        ],
-        [
             'title' => 'Maladies Chroniques : Une Crise Silencieuse',
             'content' => 'En 2016, plus de 13,7 millions de décès dans les pays à revenu faible et intermédiaire ont été causés par des maladies chroniques. Ces chiffres alarmants mettent en lumière l\'urgence d\'initiatives de prévention et de prise en charge pour réduire ce fardeau mondial. Investir dans l\'innovation peut sauver des vies.',
+        ],
+        [
+            'title' => 'Hypertension : Un Diagnostic Crucial',
+            'content' => 'En Afrique subsaharienne, une majorité alarmante de personnes souffrant d\'hypertension restent non diagnostiquées. Ce manque de diagnostic expose des millions de vies à des risques accrus de complications graves, notamment les maladies cardiovasculaires et les accidents vasculaires cérébraux.',
         ],
         [
             'title' => 'Cancer en Afrique : Une Épidémie Silencieuse',
